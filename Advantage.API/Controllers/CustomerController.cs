bbf55eecbe2fd97c8b4793fa932dbd6da9f7a699
nrholm1 <nielsraunkjaer@gmail.com>
2020-05-14 10:15:46 +0200

    Created PaginatedResponse, add Customer, Order controllers

---
+++ Advantage.API/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Advantage.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Advantage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApiContext _ctx;
+
+        public CustomerController(ApiContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = _ctx.Customers.OrderBy(c => c.Id);
+
+            return Ok(data); // return http 200 response code => succesful
+        }
+
+
+        // GET api/customers/5
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public IActionResult Get(int id)
+        {
+            var customer = _ctx.Customers.Find(id);
+
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer)
+        {
+            if (customer == null)
+                return BadRequest();
+
+            _ctx.Customers.Add(customer);
+            _ctx.SaveChanges();
+
+            return CreatedAtRoute("GetCustomer", new { id = customer.Id }, customer); // 201 response, something has been created
+        }
+
+
+    }
+}
---
+++ Advantage.API/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Advantage.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Advantage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ApiContext _ctx;
+
+        public OrderController(ApiContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        // Structure:
+        // GET api/order/pageNumber/pageSize
+        [HttpGet("{pageIndex:int}/{pageSize:int}")]
+        public IActionResult Get(int pageIndex, int pageSize)
+        {
+            var data = _ctx.Orders.Include(o => o.Customer)
+                                  .OrderByDescending(c => c.Placed);
+
+            var page = new PaginatedResponse<Order>(data, pageIndex, pageSize);
+
+            var totalCount = data.Count();
+            var totalPages = Math.Ceiling((double)totalCount / pageSize);
+
+            var response = new
+            {
+                Page = page,
+                TotalPages = totalPages
+            };
+
+            return Ok(response); // Http status 200
+        }
+    }
+}
